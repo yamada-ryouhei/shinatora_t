9bd5a3cd22df28a2b51555bdd13c79a636babb2e
yamada_ryouhei <s133094@matsu.shimane-u.ac.jp>
2015-08-26 06:52:14 +0000

    1.Text12 added Text11 updata 2.#文字が表示されるように修正 3.#Text12は　１５、１６回の内容である削除をしました 4.#この回は打つ文字数も多く確認がめんどくさいのと、サイトからアクションを 5.#サイトから探しましたが、特にエラーもなく終わることができました

---
+++ Text12.rb
@@ -0,0 +1,28 @@
+#15回目 16回
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  "adapter"=> "sqlite3",#設定
+  "database"=> "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base#ActibeRecord::Baseの内容を含んだクラスを作る
+end
+
+
+get '/' do 
+  @comments = Comment.order("id desc").all#上で作ったクラスの内容からid情報を取得させる
+  erb :index6
+  #拡張子がerbのindex4という名前のファイルを開く
+end
+
+post '/new' do 
+    Comment.create({:body => params[:body]})
+    redirect '/'
+end
+
+post '/delete' do 
+    Comment.find(params[:id]).destroy#deleteの処理をする
+end
