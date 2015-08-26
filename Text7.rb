0813eadacc1eca4e2b070e172edd3d5dd6cb68a5
yamada_ryouhei <s133094@matsu.shimane-u.ac.jp>
2015-08-26 03:11:40 +0000

    1.Text7 added 2.#複数の予想される要求に対してファイルを作ることでページを作れるようにできる 3.#共通部分は「layout」のところに書き加えて　違う部分を対応ページに書くことで省略できる 4.#エラーなどは特になし

--- Text7.rb
+++ Text7.rb
@@ -1,13 +1,23 @@
-#7回
+#8回
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/:name' do |n| #
+get '/' do #１つ目の要求と処理
   #"hello #{n}"　いつもならこんなふうに使う
-  @name = n#インスタンス変数のように宣言　下のテンプレートで使用可能になる
-  @title ="main index2"#複数作成可能
-  erb :index2
-  #拡張子がerbのindexという名前のファイルを開く
+  @title = "main index3"
+  @content ="main contend"
+  erb :index3
+  #拡張子がerbのindex3という名前のファイルを開く
 end
 
-#新しくviewsというファイルを作ればその下にファイルを作ってそこのファイルからページを作れる
+get '/about' do #２つ目の要求と種類
+  #"hello #{n}"　いつもならこんなふうに使う
+  @title = "about this page"
+  @content ="this page is ..."
+  @email="os.bb-choper-14"
+  erb :index3_2
+  #拡張子がerbのindex3という名前のファイルを開く
+end
+
+#複数のページも対応させられる
+#layoutページを作成したら共通部分を省略できる
