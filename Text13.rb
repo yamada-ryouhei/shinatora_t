ed6ec6da131a7459596c30df1b5c0eb7ad64cfe4
yamada_ryouhei <s133094@matsu.shimane-u.ac.jp>
2015-08-26 07:21:38 +0000

    1.Text13 added 2.#今回はエスケープの処理でしたが、決まり文句みたいなので覚えておきたいです 3.#エラーは出ましたがスペル問題でした

--- Text13.rb
+++ Text13.rb
@@ -8,13 +8,18 @@ ActiveRecord::Base.establish_connection(
   "database"=> "./bbs.db"
 )
 
+helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+end
+
 class Comment < ActiveRecord::Base#ActibeRecord::Baseの内容を含んだクラスを作る
 end
 
 
 get '/' do 
   @comments = Comment.order("id desc").all#上で作ったクラスの内容からid情報を取得させる
-  erb :index6
+  erb :index7
   #拡張子がerbのindex4という名前のファイルを開く
 end
 
